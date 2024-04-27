985036f340fb602180b091669733d1863ef2e62c
khang2204 <khang2242004@gmail.com>
2024-04-27 22:13:42 +0700

    Thêm AI, nhưng chưa hoàn thiện

--- DjangoWeb/DjangoApp/views.py
+++ DjangoWeb/DjangoApp/views.py
@@ -7,10 +7,10 @@ from django.contrib.auth.views import PasswordResetView
 from django.contrib.auth.views import PasswordResetDoneView 
 from django.contrib.auth.views import PasswordResetConfirmView
 from django.contrib.auth.views import PasswordResetCompleteView
-from django.http import JsonResponse
-from django.http import HttpResponse
 from DjangoApp.models import *
-import openai
+import openai,os
+from dotenv import load_dotenv
+load_dotenv()
 
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'Forget_password.html'
@@ -71,8 +71,7 @@ def ai_suggest(request):
     if request.method=="POST":
         question=request.POST.get('question')
         question= 'Bạn tên là FoodieFriend, nhiệm vụ của bạn chỉ là tư vấn về món ăn, không trả lời câu hỏi không liên quan đến món ăn, hãy đọc câu hỏi sau:'+ '"' +question+'"'+'. Nếu câu hỏi hợp lệ hãy trả lời ngắn gọn. Nếu không hợp lệ thì trả lời là: Tôi là chuyên gia về món ăn, tôi không thể trả lời những câu hỏi liên quan đến món ăn. Bạn không được dùng kí tự đặc biệt trong câu trả lời.'
-        api_key='sk-proj-g6lqMuOBL2qSxqFdCSNXT3BlbkFJadaqUmJM33RtNzsRg6FO'
-        openai.api_key=api_key
+        openai.api_key=os.getenv("OPENAI_KEY",None)
         response=openai.completions.create(
             model='gpt-3.5-turbo-instruct',
             prompt=question,
