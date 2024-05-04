d1d36c8b183e383655b89ab494229b4c97a41a28
Diephho <okdiep1@gmail.com>
2024-05-04 16:35:49 +0700

    1

---
+++ DjangoWeb/deployment.py
@@ -0,0 +1,18 @@
+import os
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ['']]
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['']]
+DEBUG = False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
--- DjangoWeb/wsgi.py
+++ DjangoWeb/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoWeb.settings')
+settings_module = 'DjangoWeb.deployment' if 'amthuclangdaihoc.azurewebsites.net' in os.environ else 'DjangoWeb.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
