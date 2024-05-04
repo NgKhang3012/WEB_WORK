69264960e561b2936363ff220a7caee8b190c8ea
Diephho <okdiep1@gmail.com>
2024-05-04 14:13:32 +0700

    fix: setting url db

--- DjangoWeb/DjangoWeb/settings.py
+++ DjangoWeb/DjangoWeb/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'DjangoWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'D:\\WEB_WORK\\DjangoWeb\\db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
